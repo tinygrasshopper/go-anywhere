a02e70849b596b46d2367bad49b847e085bf1e96
Jatin Naik <jsnaik@gmail.com>
2016-03-15 23:27:41 +0000

    wip, initial spikey idea

---
+++ goa/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err) //TODO: handle
+	}
+	//TODO: args
+
+	//TODO: try to find the file recursively till root
+	file, err := os.Open("package.path") //TODO: make filename configurable
+	if err != nil {
+		panic(err) //TODO: handle
+	}
+
+	packagePathBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err) //TODO: handle
+	}
+	packagePath := strings.TrimSpace(string(packagePathBytes))
+
+	err = os.MkdirAll(".go-anywhere/src/"+filepath.Dir(packagePath), 0744|os.ModeDir)
+	if err != nil {
+		panic(err) //TODO: handle
+	}
+	err = os.Link(pwd, ".go-anywhere/src/"+packagePath)
+	if err != nil {
+		panic(err) //TODO: handle
+	}
+
+	err = os.Setenv("GOPATH", pwd+"/.go-anywhere")
+	if err != nil {
+		panic(err) //TODO: handle
+	}
+
+	cmd := exec.Command(os.Args[0], os.Args[1:]...)
+	err = cmd.Run()
+	if err != nil {
+		panic(err) //TODO: handle
+	}
+}
