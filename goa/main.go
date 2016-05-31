f86fb6f19c59da0c5b03806971a700e35991b7ff
Jatin Naik <jsnaik@gmail.com>
2016-06-01 00:16:55 +0100

    extracted some functions

--- goa/main.go
+++ goa/main.go
@@ -9,12 +9,44 @@ import (
 )
 
 func main() {
-	pwd, err := os.Getwd()
+	var err error
+	pwd := projectRoot()
+	//TODO: args
+
+	packagePath := packagePath()
+	linkedPath := pwd + "/.go-anywhere/src/" + packagePath
+
+	ensurePath(pwd, packagePath, linkedPath)
+
+	setGoPath(pwd)
+
+	fullName, err := exec.LookPath(os.Args[1])
 	if err != nil {
 		panic(err) //TODO: handle
 	}
-	//TODO: args
 
+	runnerScript := runnerScript(linkedPath, fullName, os.Args[2:]...)
+
+	cmd := exec.Command("/bin/sh", runnerScript.Name())
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	err = cmd.Run()
+	if err != nil {
+		panic(err) //TODO: handle
+	}
+}
+
+func projectRoot() string {
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err) //TODO: handle
+	}
+	return cwd
+}
+
+func packagePath() string {
 	//TODO: try to find the file recursively till root
 	file, err := os.Open("package.path") //TODO: make filename configurable
 	if err != nil {
@@ -25,10 +57,11 @@ func main() {
 	if err != nil {
 		panic(err) //TODO: handle
 	}
-	packagePath := strings.TrimSpace(string(packagePathBytes))
-	linkedPath := pwd + "/.go-anywhere/src/" + packagePath
+	return strings.TrimSpace(string(packagePathBytes))
+}
 
-	if _, err = os.Stat(".go-anywhere"); os.IsNotExist(err) {
+func ensurePath(pwd, packagePath, linkedPath string) {
+	if _, err := os.Stat(".go-anywhere"); os.IsNotExist(err) {
 		err = os.MkdirAll(".go-anywhere/pkg/", 0744|os.ModeDir)
 		if err != nil {
 			panic(err) //TODO: handle
@@ -48,38 +81,28 @@ func main() {
 			panic(err) //TODO: handle
 		}
 	}
+}
 
-	err = os.Setenv("GOPATH", pwd+"/.go-anywhere")
+func setGoPath(pwd string) {
+	err := os.Setenv("GOPATH", pwd+"/.go-anywhere")
 	if err != nil {
 		panic(err) //TODO: handle
 	}
+}
 
-	fullName, err := exec.LookPath(os.Args[1])
-	if err != nil {
-		panic(err) //TODO: handle
-	}
-
+func runnerScript(linkedPath, fullName string, args ...string) *os.File {
 	runnerScript, err := ioutil.TempFile("", "go-anywhere-runner")
 	if err != nil {
 		panic(err) //TODO: handle
 	}
 
 	runnerScript.WriteString(`cd ` + linkedPath + "\n")
-	runnerScript.WriteString(fullName + " " + strings.Join(os.Args[2:], " "))
+	runnerScript.WriteString(fullName + " " + strings.Join(args, " "))
 
 	// TODO: Redcue permissions
 	err = runnerScript.Chmod(0700)
 	if err != nil {
 		panic(err) //TODO: handle
 	}
-
-	cmd := exec.Command("/bin/sh", runnerScript.Name())
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
-
-	err = cmd.Run()
-	if err != nil {
-		panic(err) //TODO: handle
-	}
+	return runnerScript
 }
